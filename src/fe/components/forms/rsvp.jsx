800978932dfb11d864900ad07cbd241ffea5e1b9
adnandedhawala <ddedhawala@gmail.com>
2024-01-06 14:39:28 +0530

    removed paani option

--- src/fe/components/forms/rsvp.jsx
+++ src/fe/components/forms/rsvp.jsx
@@ -105,9 +105,6 @@ export const InviteeRSVPForm = ({
             <Select.Option value="Reporting and Documentation">
               Reporting and Documentation
             </Select.Option>
-            <Select.Option value="Paani Pilawanu">
-              Paani Pilawanu during Imtehaan
-            </Select.Option>
           </Select>
         </Form.Item>
       ) : null}
