ddcd55dcc6e0aa1f6ee5db439618d467eac7608c
adnandedhawala <ddedhawala@gmail.com>
2024-01-06 13:28:31 +0530

    updated link

--- src/fe/components/headers/appHead.jsx
+++ src/fe/components/headers/appHead.jsx
@@ -4,7 +4,7 @@ export const AppHead = () => {
   return (
     <Head>
       <title>Jamea Imtehaan 1445</title>
-      <meta name="description" content="AEM RSVP" />
+      <meta name="description" content="AEM" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
